166e9f5577edfbc1e07fc9e9363fe72930689c07
sandcha <sandcha@users.noreply.github.com>
2023-07-21 14:18:08 +0200

    Initialise les entités Commune et Etat à la place de Person et Household
    Sur le modèle d'openfisca-france-dotations-locales

--- openfisca_france_territoires/entities.py
+++ openfisca_france_territoires/entities.py
@@ -8,57 +8,28 @@ See https://openfisca.org/doc/key-concepts/person,_entities,_role.html
 
 from openfisca_core.entities import build_entity
 
-Household = build_entity(
-    key = "household",
-    plural = "households",
-    label = "All the people in a family or group who live together in the same place.",
-    doc = """
-    Household is an example of a group entity.
-    A group entity contains one or more individual·s.
-    Each individual in a group entity has a role (e.g. parent or children). Some roles can only be held by a limited number of individuals (e.g. a 'first_parent' can only be held by one individual), while others can have an unlimited number of individuals (e.g. 'children').
+Commune = build_entity(
+    key = "commune",
+    plural = "communes",
+    label = "Une commune. L'entité légale la plus réduite à laquelle s'applique la législation de ce moteur de calcul.",
+    doc = '''
+    Une commune est l'entité légale la plus réduite percevant des dotations de l'État.
+    ''',
+    is_person = True,  # entité pivot
+    )
 
-    Example:
-    Housing variables (e.g. housing_tax') are usually defined for a group entity such as 'Household'.
 
-    Usage:
-    Check the number of individuals of a specific role (e.g. check if there is a 'second_parent' with household.nb_persons(Household.SECOND_PARENT)).
-    Calculate a variable applied to each individual of the group entity (e.g. calculate the 'salary' of each member of the 'Household' with salaries = household.members("salary", period = MONTH); sum_salaries = household.sum(salaries)).
-
-    For more information, see: https://openfisca.org/doc/coding-the-legislation/50_entities.html
-    """,
+Etat = build_entity(
+    key = "etat",
+    plural = "etats",
+    label = 'État',
     roles = [
         {
-            "key": "parent",
-            "plural": "parents",
-            "label": "Parents",
-            "max": 2,
-            "subroles": ["first_parent", "second_parent"],
-            "doc": "The one or two adults in charge of the household.",
-            },
-        {
-            "key": "child",
-            "plural": "children",
-            "label": "Child",
-            "doc": "Other individuals living in the household.",
-            },
-        ],
+            'key': 'commune',
+            'plural': 'communes',
+            'label': 'Communes',
+            }
+        ]
     )
 
-Person = build_entity(
-    key = "person",
-    plural = "persons",
-    label = "An individual. The minimal legal entity on which a legislation might be applied.",
-    doc = """
-
-    Variables like 'salary' and 'income_tax' are usually defined for the entity 'Person'.
-
-    Usage:
-    Calculate a variable applied to a 'Person' (e.g. access the 'salary' of a specific month with person("salary", "2017-05")).
-    Check the role of a 'Person' in a group entity (e.g. check if a the 'Person' is a 'first_parent' in a 'Household' entity with person.has_role(Household.FIRST_PARENT)).
-
-    For more information, see: https://openfisca.org/doc/coding-the-legislation/50_entities.html
-    """,
-    is_person = True,
-    )
-
-entities = [Household, Person]
+entities = [Etat, Commune]
