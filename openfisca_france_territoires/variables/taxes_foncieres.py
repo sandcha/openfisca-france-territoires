0a4f30061621c39771854a484920399cfd43a378
sandcha <sandcha@users.noreply.github.com>
2023-07-28 15:50:17 +0200

    Initie le calcul la TFB maximale N+1

--- openfisca_france_territoires/variables/taxes_foncieres.py
+++ openfisca_france_territoires/variables/taxes_foncieres.py
@@ -1,3 +1,4 @@
+from openfisca_core.model_api import max_
 from openfisca_core.periods import YEAR
 from openfisca_core.variables import Variable
 from openfisca_france_territoires.entities import Commune
@@ -7,12 +8,48 @@ from openfisca_france_territoires.entities import Commune
 # quatre taxes (ou des trois taxes pour les communes membres d’EPCI à fiscalité professionnelle unique)
 # TFB, la TFNB, la TH et la CFE
 
+
+class coefficient_variation_proportionnelle(Variable):
+    value_type = float
+    entity = Commune
+    definition_period = YEAR
+    label = "Coefficient de variation proportionnelle"
+    reference = "todo"
+
+
 class taxe_fonciere_proprietes_baties(Variable):
     value_type = float
     entity = Commune
     definition_period = YEAR
     label = "Taxe foncière sur les propriétés bâties (TFB)"
     reference = "https://www.collectivites-locales.gouv.fr/finances-locales/taxes-foncieres"
+
+class taxe_fonciere_proprietes_baties_taux_plafond(Variable):
+    value_type = float
+    entity = Commune
+    definition_period = YEAR
+    label = "Taux plafond taxe foncière sur les propriétés bâties (TFB)"
+    reference = "todo"
+
+class taxe_fonciere_proprietes_baties_taux_maximal(Variable):
+    value_type = float
+    entity = Commune
+    definition_period = YEAR
+    label = "Taxe foncière sur les propriétés bâties (TFB)"
+    reference = "https://www.collectivites-locales.gouv.fr/finances-locales/taxes-foncieres"
+    documentation = """
+    Le taux voté a l'obligation de correspondre au taux de l'année précédente 
+    multiplié par un coefficient de variation proportionnelle et ne pas dépasser 
+    un taux plafond de l'année courante.
+    """
+
+    def formula(commune, period, _parameters):
+        # TODO rendre générique toute commune
+        taux_annee_precedente = _parameters(period.last_year).taxe_fonciere_proprietes_baties.taux.chauffailles
+        coefficient_variation_proportionnelle = commune("coefficient_variation_proportionnelle", period)
+        taxe_fonciere_proprietes_baties_taux_plafond = commune("taxe_fonciere_proprietes_baties_taux_plafond", period)
+        
+        return max_(taux_annee_precedente * coefficient_variation_proportionnelle, taxe_fonciere_proprietes_baties_taux_plafond)
 
 
 class taxe_fonciere_proprietes_non_baties(Variable):
